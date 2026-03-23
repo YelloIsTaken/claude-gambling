5ff78e83d10fee8260ab0478722e70f43415b623
Yello <bartus.nowaczyk@gmail.com>
2026-03-23 11:31:18 +0100

    Update Firebase configuration with real credentials

--- config.js
+++ config.js
@@ -5,10 +5,18 @@
 // ============================================================
 
 window.FIREBASE_CONFIG = {
-  apiKey:            "PASTE_API_KEY",
-  authDomain:        "PASTE_AUTH_DOMAIN",
-  projectId:         "PASTE_PROJECT_ID",
-  storageBucket:     "PASTE_STORAGE_BUCKET",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId:             "PASTE_APP_ID"
+
+  apiKey: "AIzaSyABZ_VX9pmz80_OMuA-8f8eWw9n7X08l_Q",
+
+  authDomain: "neonbet-8388b.firebaseapp.com",
+
+  projectId: "neonbet-8388b",
+
+  storageBucket: "neonbet-8388b.firebasestorage.app",
+
+  messagingSenderId: "137314023473",
+
+  appId: "1:137314023473:web:b2ce2909b2ceca09d9bd10",
+
+  measurementId: "G-LTNWE2F6K4",
 };
